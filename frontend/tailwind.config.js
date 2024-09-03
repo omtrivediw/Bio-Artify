c1e3e94098a0ef3aec40c3476328eb0823390805
Vecforge <devansh.ps002@gmail.com>
2024-09-03 10:34:50 +0530

    Home Page Update- 1.1.6

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       backgroundImage: {
         'combo-gradient': 'linear-gradient(132deg, #0F0A30 25%, #08051A 38%)',
         'text-gradient': 'linear-gradient(90deg, #A1F596 0%, #C2EBBD 17%, #ACFCE9 35%, #A3F6FF 57%, #6AF0FF 79%, #00E5FF )',
+        'hv-text-gradient': 'linear-gradient(90deg, #00E5FF 0%, #6AF0FF 13%, #A3F6FF 35%, #ACFCE9 57%, #C2EBBD 79%, #A1F596 )',
         'radial-grad': 'radial-gradient(circle at center, #FFFFFF, #FAD0C4)',
       },
     },
