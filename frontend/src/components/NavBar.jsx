6cd88ae6def8296dbb3080d168d9a2db63f25cec
Vecforge <devansh.ps002@gmail.com>
2024-09-03 10:39:22 +0530

    Home Page Update- 1.1.8

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@
 const NavBar = () => {
   return (
     <div className='flex justify-between bg-transparent p-7 h-auto '>
-      <h1 className='text-3xl font-bold text-white font-Milonga'>BioArtify</h1>
+      <h1 className='text-3xl font-bold text-white font-Milonga hover:bg-hv-text-gradient inline-block hover:text-transparent hover:bg-clip-text cursor-pointer'>BioArtify</h1>
       <div className='flex'>
         <ul className='flex text-white gap-10 font-semibold'>
             <li>Home</li>
