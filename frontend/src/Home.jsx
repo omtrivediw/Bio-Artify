f74cda3f5c4ad40ca4209a7ca6dcf3b42183b627
Vecforge <devansh.ps002@gmail.com>
2024-09-03 10:25:11 +0530

    Home Page Update- 1.1.4

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
         <NavBar />
         <div className="flex flex-col-reverse gap-10 sm:gap-0 sm:flex-row h-auto mt-11 text-white w-4xl">
           <div className="flex flex-col justify-center gap-6 w-full p-5 mx-auto sm:h-96 sm:mx-auto sm:w-1/2 sm:p-5 bg-gray-900 rounded-[30px]">
-            <h1 className="text-3xl sm:text-5xl font-bold font-Outfit bg-text-gradient inline-block text-transparent bg-clip-text">
-              Step into the future with BioArtify.
+            <h1 className="text-3xl sm:text-5xl font-bold font-Outfit bg-text-gradient inline-block text-transparent bg-clip-text py-5">
+              Step into the future with <span className="font-Milonga ">BioArtify</span>.
             </h1>
-            <p className="mt-5 text-xl opacity-85 w-inherit flex-wrap">
+            <p className="mt-3 text-xl opacity-85 w-inherit flex-wrap">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
               earum facere consequatur natus unde quam corrupti maiores
               exercitationem et perspiciatis. Numquam est, voluptatem natus
