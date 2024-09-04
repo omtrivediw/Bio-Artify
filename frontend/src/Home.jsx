27524e2536c911fb4d08f002e122cad6a7118e4c
Vecforge <devansh.ps002@gmail.com>
2024-09-04 21:00:25 +0530

    Home Page Update- 1.1.1

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="flex flex-col-reverse gap-10 sm:gap-0 sm:flex-row h-auto mt-11 text-white w-4xl">
           <div className="flex flex-col justify-center gap-6 w-full p-5 my-10 mx-auto sm:h-96 sm:w-1/2 sm:p-5 bg-gray-900 rounded-[30px]">
             <h1 className="text-3xl sm:text-5xl font-bold font-Outfit bg-text-gradient inline-block text-transparent bg-clip-text py-5 hover:bg-hv-text-gradient cursor-default">
-              Step into the future with <span className="font-Milonga ">BioArtify</span>.
+              Step into the future with <span className="font-Milonga ">BioArtify</span>
             </h1>
             <p className="mt-3 text-xl opacity-85 w-inherit flex-wrap cursor-default">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
