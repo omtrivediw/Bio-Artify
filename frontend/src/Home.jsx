2d1232adc950067f6ab9577d262f93430ca572ca
Vecforge <devansh.ps002@gmail.com>
2024-09-12 16:48:10 +0530

    Home Page Update- 1.1.16

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,12 +4,13 @@ import Tree from "./components/Tree";
 const Home = () => {
   return (
     <div>
-      <div className="flex flex-col min-h-screen bg-combo-gradient justify-items-stretch">
+      <div className="flex flex-col min-h-screen justify-items-stretch relative">
+        <div className="absolute inset-0 bg-combo-gradient -z-10"></div> {/* Fixed background */}
         <NavBar />
         <div className="flex flex-col-reverse gap-10 sm:gap-0 sm:flex-row h-auto mt-11 text-white w-4xl">
           <div className="flex flex-col justify-center gap-6 w-full p-5 my-10 mx-auto sm:h-96 sm:w-1/2 sm:p-5 bg-gray-900 rounded-[30px]">
             <h1 className="text-3xl sm:text-5xl font-bold font-Outfit bg-text-gradient inline-block text-transparent bg-clip-text py-5 hover:bg-hv-text-gradient cursor-default">
-              Step into the future with <span className="font-Milonga ">BioArtify</span>
+              Step into the future with <span className="font-Milonga">BioArtify</span>
             </h1>
             <p className="mt-3 text-xl opacity-85 w-inherit flex-wrap cursor-default">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
@@ -17,17 +18,21 @@ const Home = () => {
               exercitationem et perspiciatis. Numquam est, voluptatem natus
               saepe ipsam iusto voluptatibus voluptate animi.
             </p>
-            <button className="mt-7 px-5 h-10 w-max bg-teal-500 font-Poetsen_One rounded-[30px] text-xl le tracking-wide hover:text-teal-500 hover:bg-white">
+            <button className="mt-7 px-5 h-10 w-max bg-teal-500 font-Poetsen_One rounded-[30px] text-xl tracking-wide hover:text-teal-500 hover:bg-white">
               Get Started
             </button>
           </div>
           <div className="flex relative justify-center w-full p-5 h-auto mx-auto sm:w-1/3">
-          <Tree />
+            <Tree />
           </div>
         </div>
-      <div>
 
+      <div className="flex h-auto justify-center mt-28">
+
+        <div className="h-[650px] mt-40 w-1/3 bg-radial-grad">
+        </div>
       </div>
+         
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'combo-gradient': 'linear-gradient(132deg, #0F0A30 25%, #08051A 38%)',
         'text-gradient': 'linear-gradient(90deg, #A1F596 0%, #C2EBBD 17%, #ACFCE9 35%, #A3F6FF 57%, #6AF0FF 79%, #00E5FF )',
         'hv-text-gradient': 'linear-gradient(90deg, #00E5FF 0%, #6AF0FF 13%, #A3F6FF 35%, #ACFCE9 57%, #C2EBBD 79%, #A1F596 )',
-        'radial-grad': 'radial-gradient(circle at center, #FFFFFF, #FAD0C4)',
+        'radial-grad': 'radial-gradient(circle 320px at center, #16555C 0%, #08051A 84%)',
       },
     },
     rotate:{
@@ -21,7 +21,10 @@ export default {
       'Outfit':'Outfit',
       'Roboto': 'Roboto',
       'Poetsen_One': '"Poetsen One"',
-    }
+    },
+    boxShadow: {
+      'custom-inset': 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset',
+    },
   },
   plugins: [],
 }
