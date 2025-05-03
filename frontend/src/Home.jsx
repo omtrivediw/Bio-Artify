65bea5f9853d4423d8a3dafc4a81a0aa60863a8f
MEISTER <devansh.ps002@gmail.com>
2025-05-03 13:42:14 +0530

    Frontend Update

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -27,12 +27,34 @@ const Home = () => {
           </div>
         </div>
 
-      <div className="flex h-auto justify-center mt-28">
+        {/*<div className="relative flex h-auto items-center mt-28">
+          <div className="flex justify-center left-0 right-0 h-48 gap-6 p-5 my-10 sm:h-96 sm:w-1/2 sm:p-5 bg-gray-900 rounded-[30px]"></div>
+          <div className="ml-auto h-[650px] mt-40 w-1/3 bg-radial-grad shadow-lg">
+          </div>  
+        </div>*/}
 
-        <div className="h-[650px] mt-40 w-1/3 bg-radial-grad">
-        </div>
-      </div>
-         
+        <div className="relative flex h-auto items-center mt-52">
+          <div style={{ backgroundColor: 'rgba(7, 5, 17, 0.6)' }} className="absolute items-center justify-center left-0 right-0 top-28 h-48 text-white p-5 bg-opacity-30 rounded-[30px] sm:h-96">
+            <h1 className="font-Outfit text-center font-bold text-4xl text- ">Contact Us Here</h1>
+          </div>
+          <div className="ml-auto h-[650px] mt-40 w-1/3 bg-radial-grad shadow-lg"></div>
+        </div>    
+
+        <div className="flex flex-col h-auto mt-0 justify-center items-center left-0 right-0 gap-6 w-full p-5 my-10 mx-auto sm:h-96 sm:w-1/2 sm:p-5 bg-gray-900 rounded-[30px]">
+          <div className="flex flex-col items-center h-fit w-fit mt-10 ">
+            <h3 className="font-Roboto font-semibold text-white text-xl border border-text-gradient pt-2 px-5 pb-2 rounded-[18px] ">Enter Prompt</h3>
+            <button className="mt-7 px-5 h-10 w-max bg-teal-500 font-Poetsen_One rounded-[30px] text-xl text-white tracking-wide hover:text-teal-500 hover:bg-white">
+              Create
+            </button>
+          </div>
+          <div className="flex h-auto justify-center gap-6 w-full p-5 my-10 mx-auto sm:h-96 sm:w-1/2 sm:p-5 bg-zinc-500 rounded-[30px]"></div>
+          <div className="flex h-auto w-full">
+            <button className="px-5 h-10 w-max border-white font-Poetsen_One rounded-[30px] text-lg text-white tracking-wide hover:text-teal-500">
+              Regenerate
+            </button>
+          </div>
+        </div>        
+
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -25,6 +25,9 @@ export default {
     boxShadow: {
       'custom-inset': 'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset',
     },
+    color:{
+      'transblac': '#070511',
+    },
   },
   plugins: [],
 }
